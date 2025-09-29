f3c8c73a344c69a77437de36287f66b0d0649f04
Aromal Baby <aromalbaby365@gmail.com>
2025-09-29 14:26:57 +0200

    Fixed the password changing bug!!!!
    a simple logic mistake was there in the profilePage

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5175'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5175', 'http://localhost:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization' ]
